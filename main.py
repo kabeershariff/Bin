2749ce3f34e5603aea0e01088fe76518cbf5df33
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2023-05-09 13:18:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from kivymd.uix.textfield import MDTextField
 from kivy.core.window import Window
 import openai
 import my_key
-import threading
 
 
 openai.api_key = my_key.my_api_key
@@ -15,32 +14,21 @@ Window.softinput_mode = 'below_target'
 class MainScreen(Screen):
     search_box = ObjectProperty()
     chat_list = ObjectProperty()
-    event = threading.Event()  # Event to synchronize threads
+    spinner = ObjectProperty()
     
     def search(self):
         self.spinner.active = True
-        self.start_background_task()
-        
-    def update_chat_list(self):    
-        
-        self.event.wait() #Wait until the answer is ready        
+    
+    
+    
+    def update(self):
+        query = self.search_box.text
         self.search_box.text = ""
         user_list_item = MDTextField(text=query, readonly=True, focus=False, mode="rectangle", icon_left="account-circle", multiline=True )
+        ai_list_item = MDTextField(text=Model.result(query), readonly=True, focus=False, mode="rectangle", icon_right="robot-happy" , multiline=True )
+        
         self.ids.chat_list.add_widget(user_list_item)
-        ai_list_item = MDTextField(text= ai_message, readonly=True, focus=False, mode="rectangle", icon_right="robot-happy" , multiline=True )
         self.ids.chat_list.add_widget(ai_list_item)
-    
-        
-    def start_background_task(self):
-        threading.Thread(target = self.answer).start()
-        
-    def answer(self, *args):
-        global query
-        global ai_message
-        query = self.search_box.text
-        ai_message = Model.result(query)
-        self.event.set() #Signal that the answer is ready
-       
 
 class ChatApp(MDApp):
     def build(self):
